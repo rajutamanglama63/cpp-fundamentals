71599bbf363f1df08024f616f93a5415da5d7531
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-10-31 17:54:12 +0545

    namespace concept

--- basics/main.cpp
+++ basics/main.cpp
@@ -9,13 +9,33 @@
 //     return 0;
 // }
 
+
+
 // const
 // it is used to protect value overriding in programe
-int main () {
-    const double PI = 3.14;
-    double radious = 22.45;
-    double circumference = 2 * PI * radious;
+// int main () {
+//     const double PI = 3.14;
+//     double radious = 22.45;
+//     double circumference = 2 * PI * radious;
 
-    std::cout << "Circumference of circle with above measuement is " << circumference << std::endl;
+//     std::cout << "Circumference of circle with above measuement is " << circumference << std::endl;
+//     return 0;
+// }
+
+
+// namespace
+// it provides a solution for preventing naming conflicts in large projects. Each entity needs a unique name.
+// A namespace allows for identically named entities as long as the namespaces are different.
+namespace first {
+    std::string name = "Raju";
+}
+
+namespace second {
+    std::string name = "Hari";
+}
+
+int main () {
+
+    std::cout << first::name << " and " << second::name << " are friends.";
     return 0;
 }
