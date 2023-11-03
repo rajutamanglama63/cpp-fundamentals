36fdb3b92992731439af1e5ce559fee1fc6b32e0
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-11-03 19:43:58 +0545

    user inputs

--- basics/main.cpp
+++ basics/main.cpp
@@ -90,7 +90,7 @@
 // type conversion
 // convert a value of one data type to another
 // there are two types of conversion in cpp and they are - Implicit (automatic) and Explicit (Preeced value with new data type like (int))
-int main () {
+// int main () {
 
     // eg. of explicit type conversion
     // double number = (int) 3.14;
@@ -98,9 +98,38 @@ int main () {
 
     // eg. of implicit type conversion
     // 100 will be implicitly convert to its asci value
-    char letter = 100;
-    std::cout << letter << std::endl;
+    // char letter = 100;
+    // std::cout << letter << std::endl;
+
+
+//     int correctMarks = 9;
+//     int totalQuestions = 10;
+
+//     double score = correctMarks/(double)totalQuestions * 100;
+
+//     std::cout << score << "%" << std::endl;
 
     
+//     return 0;
+// }
+
+
+
+
+// user input
+
+int main () {
+
+     std::string name;
+     int experience;
+
+     std::cout << "What is your name?";
+     std::cin >> name;
+
+     std::cout << "Mention your experience.";
+     std::cin >> experience;
+
+     std::cout << "Hello " << name << "!" << std::endl;
+     std::cout << "You have " << experience << " years of experience in tech field." << std::endl;
     return 0;
 }
