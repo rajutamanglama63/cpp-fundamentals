46a4802b12e29937844014480c5b16462cdccf5b
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-11-03 20:43:22 +0545

    right angle triangle program

---
+++ practice_program/findHypotenuse/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+int main () {
+
+    double perpendicular;
+    double base;
+    double hypotenuse;
+
+    std::cout << "Enter value for perpendicular: ";
+    std::cin >> perpendicular;
+
+    std::cout << "Enter value for base: ";
+    std::cin >> base;
+
+    perpendicular = pow(perpendicular, 2);
+    base = pow(base, 2);
+
+    hypotenuse = sqrt(perpendicular + base);
+
+    std::cout << "The value of hypotenuse is " << hypotenuse << std::endl;
+
+    return 0;
+}
