dfdaf09140cd854d6b23e8a573fbfc4c63f72b3c
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-11-06 13:06:42 +0545

    for loop

---
+++ basics/for-loop/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main () {
+
+    // for(int i = 1; i <= 10; i++) {
+    //     std::cout << i << std::endl;
+    // }
+
+    // for(int i = 0; i < 10; i+=2) {
+    //     std::cout << i << std::endl;
+    // }
+
+     for(int i = 10; i >= 0; i--) {
+        std::cout << i << std::endl;
+    }
+
+    std::cout << "Hello world!!" << std::endl;
+
+    return 0;
+}
