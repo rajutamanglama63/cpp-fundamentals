b53c3b08f9bfc9cd971d94edeb50e5a7f80ef731
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-11-06 11:39:39 +0545

    tempreature conversion program

---
+++ practice_program/temp-conversion-program/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main () {
+
+    double temp;
+    char unit;
+
+
+    std::cout << "******************* Temprature conversion program *******************" << std::endl;
+
+    std::cout << "F = Fehrenhiet" << std::endl;
+    std::cout << "C = Celcius" << std::endl;
+
+    std::cout << "What unit would like to convert to: ";
+    std::cin >> unit;
+
+    if(unit == 'F' || unit == 'f') {
+        std::cout << "Enter temprature in calcius: ";
+        std::cin >> temp;
+
+        temp = (1.8 * temp) +32.0;
+        std::cout << "Temprature is: " << temp << " F" << std::endl;
+    } else if (unit == 'C' || unit == 'c') {
+        std::cout << "Enter temprature in fehrenheit: ";
+        std::cin >> temp;
+
+        temp = (temp -32) / 1.8;
+        std::cout << "Temprature is: " << temp << " C" << std::endl;
+    }
+
+
+    std::cout << "*********************************************************************" << std::endl;
+
+    return 0;
+}
