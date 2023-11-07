3af31245bec4f7ba9b421f55a4a00c114784bb4c
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-11-07 16:23:11 +0545

    parameter & argument

--- basics/function/main.cpp
+++ basics/function/main.cpp
@@ -2,15 +2,19 @@
 
 // function - a block of reusable code
 
-void birthdayWish () {
-    std::cout << "Happy birthday to you" << std::endl;
-}
+void birthdayWish(std::string name);
 
 int main () {
 
-    birthdayWish();
-    birthdayWish();
-    birthdayWish();
+    std::string name = "Raju";
+
+    birthdayWish(name);
+    birthdayWish(name);
+    birthdayWish(name);
 
     return 0;
+}
+
+void birthdayWish (std::string name) {
+    std::cout << "Happy birthday to " << name << std::endl;
 }
