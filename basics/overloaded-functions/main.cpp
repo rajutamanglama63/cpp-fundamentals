c1170006e54e47a950bfdc06a71bfeab72738031
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-11-07 16:57:17 +0545

    overloaded function

---
+++ basics/overloaded-functions/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void bakePizza ();
+void bakePizza (std::string topping1);
+void bakePizza (std::string topping1, std::string topping2);
+
+int main () {
+
+    bakePizza();
+    bakePizza ("pepperoni");
+    bakePizza ("pepperoni", "mushroom");
+
+    return 0;
+}
+
+void bakePizza () {
+    std::cout << "Here is your pizza" << std::endl;
+}
+
+void bakePizza (std::string topping1) {
+    std::cout << "Here is your " << topping1 << " pizza" << std::endl;
+}
+
+void bakePizza (std::string topping1, std::string topping2) {
+    std::cout << "Here is your " << topping1 << " " <<  topping2 << " pizza" << std::endl;
+}
